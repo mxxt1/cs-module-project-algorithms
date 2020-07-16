1ea7564ee73edeb4b99b1e9003a545dcb5670b70
Matt Gill <matt@dustcloudlabs.com>
2020-07-16 15:22:34 -0400

    single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,6 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # loop through array and count instances of the element, if the element only shows up once it's the single element
+    # loop through the array using a hash table to cache repeating numbers, if number exists in table increment count +1, don't add new entry, move to next number. if number doesn't exist in table, move forward. If we hit the end, the number with 1 instance is the number
+    
+    # I could have done this with a list lol
+    cache = {}
+    
+    for number in arr:
+        if number in cache:
+            del cache[number]
+        else:
+            cache[number] = 1
+    return list(cache.keys())[0]
+    
+
+
 
     pass
 
